e1dbebc97afbeb229d168dc1894e2255419f541f
TsembA <tsembenhoi@gmail.com>
2025-12-27 15:19:22 -0800

    chore(terraform): switch EC2 provisioning to SSM (remove user_data)

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -8,16 +8,10 @@ resource "aws_instance" "app" {
   key_name             = "nodejs-shopping-key"
   iam_instance_profile = aws_iam_instance_profile.ec2_ssm_profile.name
 
-  user_data = file("${path.module}/user_data.sh")
-
   tags = {
     Name    = var.instance_name
     Project = "nodejs-shopping"
     Managed = "terraform"
-  }
-
-  lifecycle {
-    ignore_changes = [user_data]
   }
 }
 
