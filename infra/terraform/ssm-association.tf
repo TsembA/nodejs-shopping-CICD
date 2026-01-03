2a1745b61c98634ff1462f149cfc98c6a37fdb46
TsembA <tsembenhoi@gmail.com>
2026-01-02 22:23:29 -0800

    fix(infra): add SSM association to trigger EC2 deployment

---
+++ infra/terraform/ssm-association.tf
@@ -0,0 +1,16 @@
+// FILE: infra/terraform/ssm-association.tf
+
+resource "aws_ssm_association" "deploy_app" {
+  name = aws_ssm_document.deploy.name
+
+  targets {
+    key    = "InstanceIds"
+    values = [aws_instance.app.id]
+  }
+
+  depends_on = [
+    aws_instance.app,
+    aws_iam_instance_profile.this,
+    aws_ssm_document.deploy
+  ]
+}
