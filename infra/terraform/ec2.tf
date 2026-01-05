929451a50e58fef64ad1c0a640135585ac6ffad1
TsembA <tsembenhoi@gmail.com>
2026-01-04 19:03:24 -0800

    fix(ansible): correctly group EC2 instances by Role tag in dynamic inventory

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "app" {
 
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.app.id]
-
+  
+  associate_public_ip_address = true
   key_name = aws_key_pair.app.key_name
 
   tags = {
